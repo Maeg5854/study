660b5a9832f47a365a3f13b8d0abb4fe51a0f1a0
Maeg5854 <kimhj585407@gmail.com>
2024-10-13 22:03:06 +0900

    [tobi] 6.3 일반적인 데코레이터 패턴으로 작성한 프록시의 문제
    - 모든 메서드에 대해서 구현해야함.
    - 기능이 중복해서 나타난다.

---
+++ tobispring/src/test/java/org/mandy/tobi/learn/proxy/HelloClientTest.java
@@ -0,0 +1,21 @@
+package org.mandy.tobi.learn.proxy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloClientTest {
+
+    @Test
+    public void simpleProxy() {
+        Hello hello = new HelloTarget();
+        assertThat(hello.sayHello("Toby")).isEqualTo("Hello Toby");
+        assertThat(hello.sayHi("Toby")).isEqualTo("Hi Toby");
+        assertThat(hello.sayThankYou("Toby")).isEqualTo("Thank You Toby");
+
+        Hello proxyHello = new HelloUppercase(new HelloTarget());
+        assertThat(proxyHello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");
+        assertThat(proxyHello.sayHi("Toby")).isEqualTo("HI TOBY");
+        assertThat(proxyHello.sayHello("Toby")).isEqualTo("HELLO TOBY");
+    }
+}
---
+++ tobispring/src/test/java/org/mandy/tobi/learn/proxy/HelloTarget.java
@@ -0,0 +1,19 @@
+package org.mandy.tobi.learn.proxy;
+
+public class HelloTarget implements Hello {
+
+    @Override
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+
+    @Override
+    public String sayHi(String name) {
+        return "Hi " + name;
+    }
+
+    @Override
+    public String sayThankYou(String name) {
+        return "Thank You " + name;
+    }
+}
